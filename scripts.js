6e1baf1d439f4340c8cadd1359afb31119e340cb
Monika Łasak <monika.k.lasak@gmail.com>
2018-04-15 18:41:46 +0200

    Change in scripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /// scripts.js
-var a=5;
-	h=14;
-	triangleArea= prompt "a*h/2";
+var a=5,
+	h=14,
+	triangleArea= prompt "a*h/2",
 
 	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
