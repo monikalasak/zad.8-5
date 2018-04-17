319e6845d5eedb56e99e6afda99a1e05b63dae1c
Monika Łasak <monika.k.lasak@gmail.com>
2018-04-17 17:39:51 +0200

    Change javascripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /// scripts.js
 var a=5,
 	h=14,
-	triangleArea= prompt "a*h/2",
+	triangleArea= "a*h/2";
 
 	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
